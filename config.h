5cb0d06ce8c02e293c314a612b290a3eb2cdd3b2
Roman Borisov <rborisoff@gmail.com>
2016-11-13 20:46:44 +0000

    increased space

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 #define _MAXSTRING_ 128
 #define _HOST_ "localhost"
 #define _PORT_ 9213
-#define _FS_BYTES_AVAIL_ 52428800
+#define _FS_BYTES_AVAIL_ 252428800
 //#define _FS_BYTES_AVAIL_ 137929368000
 #define _DB_PATH_ "/storage"
 #define _FS_SLEEP_ 15
